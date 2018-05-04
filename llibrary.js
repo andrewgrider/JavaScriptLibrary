3f53b4e1cd2cf08ef88a39b3b360b9dcc7837bfe
Andrew G <38119960+andrewgrider@users.noreply.github.com>
2018-05-04 10:24:38 -0400

    Update llibrary.js

--- llibrary.js
+++ llibrary.js
@@ -1,12 +1,42 @@
-class Library {
-  constructor(elem){
-    this.helper = {
-      "elem" : elem ? elem : null,
-      
-    };
-  }
-  get now(){
+function Library(selector) {
+  this.ref = {
+    "index" : null
+  };
+  this.select = function(el){
+    this.ref.index = null;
+    switch(el){
+      case "body":
+        return document.body;
+        break;
+      default:
+        if(typeof(el) === typeof('String')){
+          if(el.match(/\.|\#/)){
+            if(this.ref.index){
+              return document.querySelectorAll(el)[this.ref.index];
+            } else {
+              return document.querySelectorAll(el);
+            }
+          } else {
+            if(this.ref.index){
+              return document.getElementsByTagName(el)[this.ref.index];
+            } else {
+              return document.getElementsByTagName(el);
+            }
+          }
+        } else {
+          return el;
+        }
+        break;
+    }
+  };
+  this.helper = {
+    elem : selector ? this.select(selector) : null
+  };
+  this.now = function(){
     return new Date.now();
-  }
+  };
+  this.eq = function(index){
+    this.ref.index = index;
+    return this;
+  };
 }
-window.$ = new Library();
